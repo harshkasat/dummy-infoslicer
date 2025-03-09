2fd30a7a1caf7ff76e3e9616bb6c50b5f5a25ba8
Zedmat <104870914+harshkasat@users.noreply.github.com>
2025-03-09 19:19:20 +0530

    adding missing package and install it

--- infoslicer/processing/media_wiki_Parser.py
+++ infoslicer/processing/media_wiki_Parser.py
@@ -12,7 +12,7 @@ class MediaWiki_Parser(HTMLParser):
     remove_classes_regexp = re.compile("toc|noprint|metadata|sisterproject|boilerplate|reference(?!s)|thumb|navbox|editsection")
 
     def __init__(self, document_to_parse, title, source_url):
-        logger.error("Revid" + document_to_parse['parse']['revid'])
+        # logger.error("Revid" + document_to_parse['parse']['revid'])
         logger.error(document_to_parse[:400])
         revid = '1278607833'
         if document_to_parse is None:
